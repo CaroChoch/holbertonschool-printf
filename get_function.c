cdfa7985a846c974b9f7d562a946bd0396ff7579
CaroChoch <5692@holbertonstudents.com>
2022-11-11 09:14:27 -0800

    fix bug

--- get_function.c
+++ get_function.c
@@ -34,21 +34,21 @@ int get_function(const char *format, print_type argument[], va_list argp)
 					break;
 				}
 			}
-<<<<<<< HEAD
+
 			i++;
 			if (!argument[j].type)
 			{
 				count += _putchar('%');
 				count += _putchar(format[i]);
 			}
-=======
+
 		    i++;
 		    if (!argument[j].type)
 		    {
 			    count += _putchar('%');
 			    count += _putchar(format[i]);
 		    }
->>>>>>> Caro
+
 	}
 	return (count);
 }
